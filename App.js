6c0839dd0c5f96b8ff15ae7f5f6b495353830f68
Ryanhustler420 <gouravgupta840@gmail.com>
2024-08-28 19:36:48 +0530

    fixed the screen display style

--- App.js
+++ App.js
@@ -69,8 +69,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   label: {
     fontFamily: "regular"
--- screens/ChatListScreen.js
+++ screens/ChatListScreen.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native'; 
+import { View, Text, StyleSheet } from 'react-native'; 
 
 const ChatListScreen = props => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Chat List Screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
 
 export default ChatListScreen;
