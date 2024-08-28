0bc02831a40209a11c8aa74bb9f4fcac723de1a8
Ryanhustler420 <gouravgupta840@gmail.com>
2024-08-28 19:07:29 +0530

    setting custom fonts

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import { SafeAreaProvider  } from "react-native-safe-area-context";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { useState, useEffect, useCallback } from 'react';
 import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -10,10 +11,33 @@ export default function App() {
   const [appIsLoaded, setAppIsLoaded] = useState(false);
 
   useEffect(() => {
-    // Load fonts
-    setTimeout(() => {
-      setAppIsLoaded(true);
-    }, 1000);
+    // https://docs.expo.dev/versions/latest/sdk/splash-screen/
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          "black": require("./assets/fonts/Roboto-Black.ttf"),
+          "blackItalic": require("./assets/fonts/Roboto-BlackItalic.ttf"),
+          "bold": require("./assets/fonts/Roboto-Bold.ttf"),
+          "boldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
+          "italic": require("./assets/fonts/Roboto-Italic.ttf"),
+          "light": require("./assets/fonts/Roboto-Light.ttf"),
+          "lightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
+          "medium": require("./assets/fonts/Roboto-Medium.ttf"),
+          "mediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
+          "regular": require("./assets/fonts/Roboto-Regular.ttf"),
+          "thin": require("./assets/fonts/Roboto-Thin.ttf"),
+          "thinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf"),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsLoaded(true);
+      }
+    }
+
+    prepare();
+
   }, []);
 
   const onLayout = useCallback(async () => {
@@ -26,7 +50,7 @@ export default function App() {
 
   return (
     <SafeAreaProvider style={styles.container} onLayout={onLayout}>
-      <Text>Hello, World</Text>
+      <Text style={styles.label}>Hello, World</Text>
     </SafeAreaProvider>
   );
 }
@@ -38,4 +62,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  label: {
+    fontFamily: "regular"
+  }
 });
