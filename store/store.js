fa14ff1ffbf184a5bbd0c3babefe10a58b24b1d7
Ryanhustler420 <gouravgupta840@gmail.com>
2024-08-30 11:29:39 +0530

    configure out auth slice

--- store/store.js
+++ store/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import authSlice from './authSlice';
+
 // https://redux-toolkit.js.org/tutorials/quick-start#create-a-redux-store
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authSlice
+  },
 });
