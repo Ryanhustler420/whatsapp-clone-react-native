7a72d1dbe11eb2fee42af0a326ceaca1f7ca7c15
Ryanhustler420 <gouravgupta840@gmail.com>
2024-08-31 20:21:32 +0530

    showing a message when no results were found

--- screens/NewChatScreen.js
+++ screens/NewChatScreen.js
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Button, TextInput } from 'react-native'; 
 import CustomHeaderButton from '../components/CustomHeaderButton';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import PageContainer from "../components/PageContainer";
 import { Ionicons } from '@expo/vector-icons';
 import Colors from '../constants/colors';
+import CommonStyles from "../constants/commonStyles";
 
 const NewChatScreen = props => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [noResultFound, setNoResultFound] = useState();
+  const [users, setUsers] = useState();
 
   useEffect(() => {
     props?.navigation?.setOptions({
@@ -32,6 +37,32 @@ const NewChatScreen = props => {
           onChangeText={() => {}}
         />
       </View>
+      {
+        !isLoading && noResultFound && (
+          <View style={CommonStyles.center}>
+            <Ionicons 
+              color={Colors.lightGrey} 
+              name="airplane-outline" 
+              size={55} 
+              style={styles.noResultIcon}
+            />
+            <Text style={styles.noResultText}>No users found!</Text>
+          </View>
+        )
+      }
+      {
+        !isLoading && !users && (
+          <View style={CommonStyles.center}>
+            <Ionicons 
+              color={Colors.lightGrey} 
+              name="person-outline" 
+              size={55} 
+              style={styles.noResultIcon}
+            />
+            <Text style={styles.noResultText}>Enter a name to search for user!</Text>
+          </View>
+        )
+      }
     </PageContainer>
   );
 }
@@ -51,6 +82,14 @@ const styles = StyleSheet.create({
     marginLeft: 8,
     fontSize: 15,
     width: 100,
+  },
+  noResultIcon: {
+    marginBottom: 20,
+  },
+  noResultText: {
+    color: Colors.textColor,
+    fontFamily: 'regular',
+    letterSpacing: .3,
   }
 });
 
