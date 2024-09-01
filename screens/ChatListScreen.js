1899583a27ccc67fa072f7989aa3cb4c247b7e77
Ryanhustler420 <gouravgupta840@gmail.com>
2024-09-01 17:51:22 +0530

    outputting the user data

--- screens/ChatListScreen.js
+++ screens/ChatListScreen.js
@@ -3,6 +3,9 @@ import { Text, StyleSheet, FlatList } from 'react-native';
 import CustomHeaderButton from '../components/CustomHeaderButton';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
+import DataItem from '../components/DataItem';
+import PageContainer from '../components/PageContainer';
+import PageTitle from '../components/PageTitle';
 
 const ChatListScreen = props => {
 
@@ -11,12 +14,10 @@ const ChatListScreen = props => {
   const authData = useSelector((state) => state.auth.userData);
   const userChats = useSelector((state) => {
     const chatsData = state.chats.chatsData;
-    return Object.values(chatsData);
+    return Object.values(chatsData).sort((a, b) => {
+      return new Date(b.updatedAt) - new Date(a.updatedAt);
+    });
   });
-
-  console.log(storedUsers);
-
-  console.log(userChats);
 
   useEffect(() => {
     props?.navigation?.setOptions({
@@ -48,14 +49,29 @@ const ChatListScreen = props => {
 
   }, [props.route?.params]);
 
-  return <FlatList
-    data={userChats}
-    renderItem={(itemData) => {
-      const chatData = itemData.item;
-      const otherUserId = chatData.users.find(uid => uid != authData.userId);
-      return <Text>{otherUserId}</Text>
-    }}
-  />;
+  return <PageContainer style={{ backgroundColor: 'white' }}>
+    <PageTitle text="Chats" />
+    <FlatList
+      data={userChats}
+      renderItem={(itemData) => {
+        const chatData = itemData.item;
+        const otherUserId = chatData.users.find(uid => uid != authData.userId);
+        const otherUser = storedUsers[otherUserId];
+
+        if (!otherUser) return;
+
+        const title = `${otherUser.firstName} ${otherUser.lastName}`;
+        const subTitle = "This will be a message...";
+        // const image = otherUser.profilePicture;
+
+        return <DataItem 
+          title={title}
+          subTitle={subTitle}
+          // image={image}
+        />
+      }}
+    />
+  </PageContainer>;
 }
 
 const styles = StyleSheet.create({
