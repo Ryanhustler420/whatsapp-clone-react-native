8e62e20e57a78faf309d99527ba78ace1778b0f9
Ryanhustler420 <gouravgupta840@gmail.com>
2024-09-02 12:36:33 +0530

    adding and removing starred messages in the app state

--- store/messagesSlice.js
+++ store/messagesSlice.js
@@ -4,6 +4,7 @@ const messagesSlice = createSlice({
   name: "messages",
   initialState: {
     messagesData: {},
+    starredMessages: {},
   },
   reducers: {
     setChatMessages: (state, action) => {
@@ -14,8 +15,22 @@ const messagesSlice = createSlice({
 
       state.messagesData = existingMessages;
     },
+    addStarredMessage: (state, action) => {
+      const { starredMessageData } = action.payload;
+      state.starredMessages[starredMessageData.messageId] = starredMessageData;
+    },
+    removeStarredMessage: (state, action) => {
+      const { messageId } = action.payload;
+      delete state.starredMessages[messageId];
+    },
+    setStarredMessages: (state, action) => {
+      const { starredMessages } = action.payload;
+      state.starredMessages = { ...starredMessages };
+    },
   }
 });
 
 export const setChatMessages = messagesSlice.actions.setChatMessages;
+export const addStarredMessage = messagesSlice.actions.addStarredMessage;
+export const removeStarredMessage = messagesSlice.actions.removeStarredMessage;
 export default messagesSlice.reducer;
